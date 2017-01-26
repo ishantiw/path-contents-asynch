b12b727da1870dde011520f369aaf49858588555
!shan <ishan210788@gmail.com>
2017-01-26 17:05:13 +0100

    adding all tests

--- test/test.js
+++ test/test.js
@@ -1,53 +1,50 @@
-'use strict';
-
-var expect = require('chai').expect;
-var chai = require('chai');
-var pathContentsAsynch = require('../index');
-var mock = require('mock-fs');
-var chaiAsPromised = require('chai-as-promised');
+/**
+* Path Contents Asynch Testing: A module to test all the relevant functionality and results of pathContentsAsynch module
+* @author Ishan Tiwari <ishan210788@gmail.com>
+*/
+const expect = require('chai').expect;
+const chai = require('chai');
+const pathContentsAsynch = require('../index');
+const mock = require('mock-fs');
+const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 
 describe('#Check all files and folders', function() {
 
-/* Can also be done using mocks */
 
-  mock({
-    'path/to/fake/dir': {
-      'some-file.txt': 'This is a testing file',
-      'foo': {/* empty directory*/}
-    },
-    'path/to/test.png': new Buffer([8, 6, 7, 5, 3, 0, 9]),
-    'some/other/path': {/* another empty directory */}
+  let pathContents = pathContentsAsynch('test/foo').then(function (result) {
+    return result;
   });
 
     it('checking if there is any such dir', function() {
 
-      pathContentsAsynch('test/foo').then((results)=> {
-        console.log(results);
-        expect(results).to.not.equal(null);
-      });
-      mock.restore();
+      expect(pathContents).to.eventually.not.equal(null);
+
     }),
 
     it('check if it has \'dir\' property', function() {
 
-      return expect(pathContentsAsynch('test/foo')).to.eventually.have.property('dirs');
+      expect(pathContents).to.eventually.have.property('dirs');
 
     }),
 
     it('check if it has \'files\' property', function() {
 
-      return expect(pathContentsAsynch('test/foo')).to.eventually.have.property('files');
+      expect(pathContents).to.eventually.have.property('files');
 
     }),
 
-    it('check the number of dirs', function() {
+    it('checking the number of dirs', function() {
 
-      return expect(pathContentsAsynch('test/foo').then((result)=> {
-        return result.dirs.length;
-      })).to.be.fulfilled.and.eventually.equal(6);
+      expect(pathContents).to.eventually.have.property('dirs').with.lengthOf(2);
 
-    })
+    }),
+
+    it('checking the number of files', function() {
+
+      expect(pathContents).to.eventually.have.property('files').with.lengthOf(3);
+
+    });
 
   });
